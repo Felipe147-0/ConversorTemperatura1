32297a0ede9adf15a7746fa10becc3f10ac1639b
Felipe <fepaganini6@gmail.com>
2024-11-17 22:08:22 -0300

    I. ConversorTemperatura

--- app/src/main/java/br/edu/ifsp/dmo/conversordetemperatura1/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/conversordetemperatura1/view/MainActivity.kt
@@ -1,10 +1,9 @@
-package br.edu.ifsp.dmo.conversordetemperatura1
+package br.edu.ifsp.dmo.conversordetemperatura1.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.dmo.conversordetemperatura1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
